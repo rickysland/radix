82cb4d329d32a8c60d1930ce1bf4dc979a39f15f
rickysland <rickyli@163.com>
2019-06-03 15:46:09 +0800

    Update stream.go

--- stream.go
+++ stream.go
@@ -10,9 +10,9 @@ import (
 	"strconv"
 	"time"
 
-	"github.com/mediocregopher/radix/v3/internal/bytesutil"
-	"github.com/mediocregopher/radix/v3/resp"
-	"github.com/mediocregopher/radix/v3/resp/resp2"
+	"github.com/rickysland/radix/v3/internal/bytesutil"
+	"github.com/rickysland/radix/v3/resp"
+	"github.com/rickysland/radix/v3/resp/resp2"
 )
 
 // StreamEntryID represents an ID used in a Redis stream with the format <time>-<seq>.
